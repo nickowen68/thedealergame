7876f34d7f78dd19a2a6211d1892a5b754173fcf
nickowen68 <52604033+nickowen68@users.noreply.github.com>
2019-07-31 22:19:00 +0300

    Update sendmail.php

--- sendmail.php
+++ sendmail.php
@@ -1,9 +1,9 @@
 <?php
-if(isset($_POST['submit'])) {
-	$sender=$_POST['name'];
-    $subject=$_POST['subject'];
-    $senderEmail=$_POST['email'];
-    $message=$_POST['message'];
+if(isset($_GET['submit'])) {
+	$sender=$_GET['name'];
+    $subject=$_GET['subject'];
+    $senderEmail=$_GET['email'];
+    $message=$_GET['message'];
 	
 	$recipient="dealgame.contact@gmail.com";
     $mailBody="Name: $sender\nEmail Address: $senderEmail\n\nMessage: $message";
