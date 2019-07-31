02b6d25e8503505a6b280284085b1b75ff39404f
nickowen68 <52604033+nickowen68@users.noreply.github.com>
2019-07-31 22:14:53 +0300

    Add files via upload

---
+++ sendmail.php
@@ -0,0 +1,14 @@
+<?php
+if(isset($_POST['submit'])) {
+	$sender=$_POST['name'];
+    $subject=$_POST['subject'];
+    $senderEmail=$_POST['email'];
+    $message=$_POST['message'];
+	
+	$recipient="dealgame.contact@gmail.com";
+    $mailBody="Name: $sender\nEmail Address: $senderEmail\n\nMessage: $message";
+	
+    mail($recipient, $subject, $mailBody);
+    header("Location: http://thedealergame.com/");
+}
+?>
